e1a8ce88312ff483120755dd51b9aade3b06fad3
CarterBloop <carter@tiogaventures.com>
2022-11-30 12:47:30 -0800

    Player class

---
+++ Assets/Scripts/PlayerUnit.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUnit : MonoBehaviour
+{
+    public int maxHP;
+    public int currentHP;
+
+    public string color;
+    public int position;
+}
