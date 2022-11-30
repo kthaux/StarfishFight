149e480460a0b4b040ba99d5383e4cbf150b4a14
CarterBloop <carter@tiogaventures.com>
2022-11-29 20:48:37 -0800

    BattleState and BattleSystem

---
+++ Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        SetupBattle();
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
