e33fab53238e127b944ccc99f43f382f6391b55e
CarterBloop <carter@tiogaventures.com>
2022-11-30 13:22:07 -0800

    PlayerUnit Class

--- Assets/Scripts/PlayerUnit.cs
+++ Assets/Scripts/PlayerUnit.cs
@@ -4,9 +4,41 @@ using UnityEngine;
 
 public class PlayerUnit : MonoBehaviour
 {
-    public int maxHP;
+    public int maxHP = 5;
     public int currentHP;
 
-    public string color;
-    public int position;
+    public string attackColor;
+    public string timeColor;
+    public string moveColor;
+    public Vector2Int position;
+
+    public void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+        if (currentHP <= 0)
+        {
+            currentHP = 0;
+            Debug.Log("Unit has died");
+        }
+    }
+
+    public void SetPosition(Vector2Int pos)
+    {
+        position = pos;
+    }
+
+    public void SetAttackColor(string color)
+    {
+        attackColor = color;
+    }
+
+    public void SetTimeColor(string color)
+    {
+        timeColor = color;
+    }
+
+    public void SetMoveColor(string color)
+    {
+        moveColor = color;
+    }
 }
