31c4f8276c92a6fcc6c8a3bc8a723999d9d7c658
Jackson <deltarax@gmail.com>
2022-12-01 12:09:44 -0800

    Added each limb prefab

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class GameHandler : MonoBehaviour
 {
-    //Creates the base limb prefab
-    public GameObject limb;
+    //Creates the base leftLimb prefab
+    public GameObject leftLimb;
+    public GameObject topLimb;
+    public GameObject rightLimb;
+    public GameObject botLeftLimb;
+    public GameObject botRightLimb;
 
     void Start()
     {
         // Fuckin Jank way to spawn in 5 limbs at random locations
-        Vector2 position1 = new Vector2(Random.Range(-10, 10), Random.Range(-10, 10));
-        GameObject Limb1 = Instantiate(limb, position1, Quaternion.identity);
-        Vector2 position2 = new Vector2(Random.Range(-10, 10), Random.Range(-10, 10));
-        GameObject Limb2 = Instantiate(limb, position2, Quaternion.identity);
-        Vector2 position3 = new Vector2(Random.Range(-10, 10), Random.Range(-10, 10));
-        GameObject Limb3 = Instantiate(limb, position3, Quaternion.identity);
-        Vector2 position4 = new Vector2(Random.Range(-10, 10), Random.Range(-10, 10));
-        GameObject Limb4 = Instantiate(limb, position4, Quaternion.identity);
-        Vector2 position5 = new Vector2(Random.Range(-10, 10), Random.Range(-10, 10));
-        GameObject Limb5 = Instantiate(limb, position5, Quaternion.identity);
+        Vector2 position1 = new Vector2(Random.Range(-9, 9), Random.Range(-9, 9));
+        GameObject Limb1 = Instantiate(leftLimb, position1, Quaternion.identity);
+        Vector2 position2 = new Vector2(Random.Range(-9, 9), Random.Range(-9, 9));
+        GameObject Limb2 = Instantiate(topLimb, position2, Quaternion.identity);
+        Vector2 position3 = new Vector2(Random.Range(-9, 9), Random.Range(-9, 9));
+        GameObject Limb3 = Instantiate(rightLimb, position3, Quaternion.identity);
+        Vector2 position4 = new Vector2(Random.Range(-9, 9), Random.Range(-9, 9));
+        GameObject Limb4 = Instantiate(botLeftLimb, position4, Quaternion.identity);
+        Vector2 position5 = new Vector2(Random.Range(-9, 9), Random.Range(-9, 9));
+        GameObject Limb5 = Instantiate(botRightLimb, position5, Quaternion.identity);
 
     }
 
