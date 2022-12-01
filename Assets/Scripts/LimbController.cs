a5983016260b4a83be8f870ff0a8b16cf50c9939
Jackson <deltarax@gmail.com>
2022-12-01 13:12:28 -0800

    Limbs move with body when collected

--- Assets/Scripts/LimbController.cs
+++ Assets/Scripts/LimbController.cs
@@ -6,6 +6,8 @@ public class LimbController : MonoBehaviour
 {
    //Keep track of total picked up limbs
     public static int totalLimbs = 0; 
+    public bool collected = false;
+    public Vector2 speed = new Vector2(10, 10);
 
     void Awake()
     {
@@ -21,9 +23,27 @@ public class LimbController : MonoBehaviour
             //Add limb to counter
             totalLimbs++;
             //Test: Print total number of limbs
+            Debug.Log("You collected: " + this);
             Debug.Log("You currently have " + LimbController.totalLimbs + " limbs.");
             //Destroy limb
-            Destroy(gameObject);
+            // Destroy(gameObject);
+            // transform.Translate(c2d.position);
+            this.collected = true;
         }
+    }
+
+    void Update()
+    {
+        // Sets the limb to move at the same rate as the body, effectivly "combining" them
+       if (collected) {
+            float inputX = Input.GetAxis("Horizontal");
+            float inputY = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
+
+            movement *= Time.deltaTime;
+
+            transform.Translate(movement);
+       } 
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Vector2 speed = new Vector2(50, 50);
+    public Vector2 speed = new Vector2(10, 10);
 
     // Update is called once per frame
     void Update()
