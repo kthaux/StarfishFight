45d74539342117628258b28525bb606af395ea00
kthaux <kthaux@ucsc.edu>
2022-12-01 15:37:30 -0800

    Renamed sample scene. moving between scenes now works. still need object cleanup in minigame

--- Assets/Scripts/minigame/GameHandler.cs
+++ Assets/Scripts/minigame/GameHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameHandler : MonoBehaviour
 {
@@ -16,8 +17,15 @@ public class GameHandler : MonoBehaviour
     public GameObject starmini2;
     public GameObject starmini3;
 
+    private GameObject go;
+
+    public static bool minidone = true;
+
+    public float timer = 5;
+
     void Start()
     {
+        minidone = false;
         // Fuckin Jank way to spawn in 5 limbs at random locations
         Vector2 position1 = new Vector2(Random.Range(-9, 9), Random.Range(-9, 9));
         GameObject Limb1 = Instantiate(leftLimb, position1, Quaternion.identity);
@@ -30,6 +38,8 @@ public class GameHandler : MonoBehaviour
         Vector2 position5 = new Vector2(Random.Range(-9, 9), Random.Range(-9, 9));
         GameObject Limb5 = Instantiate(botRightLimb, position5, Quaternion.identity);
 
+        //initiate timer based off of legs
+        timer = 10;
 
         //choose the appropriate starfish to spawn
 
@@ -41,6 +51,14 @@ public class GameHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(timer <= 0)
+        {
+
+            minidone = true;
+            SceneManager.UnloadSceneAsync(2);
+        }
+        timer -= Time.deltaTime;
+        Debug.Log("Timer: " + timer);
+
     }
 }
