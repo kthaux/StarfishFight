1c3d215575a29018d7941cdf6f6197d3ac9eac1e
CarterBloop <carter@tiogaventures.com>
2022-12-01 17:55:56 -0800

    Music

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioSource musicSource;
+    public AudioSource musicsource;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
